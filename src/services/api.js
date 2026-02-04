9805f535e4fb8e81d9f5c83b47bc91db4c20cd4d
ssetthichaywra-droid <ssetthichaywra@gmail.com>
2026-02-04 14:24:40 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = 'https://backend-vs1n.onrender.com/+-';
 
 const api = axios.create({
     baseURL: BASE_URL,
